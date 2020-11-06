54b2867f1e89caef3014c1f83d051761a4d8655d
Michael Lodder <redmike7@gmail.com>
2020-11-06 16:13:19 -0700

    feat(rust): update create proof
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/android.rs
+++ src/android.rs
@@ -19,9 +19,11 @@ use bbs::{ToVariableLengthBytes, FR_COMPRESSED_SIZE, G1_COMPRESSED_SIZE, G2_COMP
 use crate::{bls_generate_blinded_g2_key, bls_generate_blinded_g1_key, bls_generate_g2_key, bls_generate_g1_key};
 use crate::bbs_sign::*;
 use crate::bls::{
-    bls_secret_key_size, bls_public_key_g1_size, bls_public_key_g2_size, bls_get_public_key};
+    bls_secret_key_size, bls_public_key_g1_size, bls_public_key_g2_size};
 use crate::bbs_blind_commitment::{bbs_blind_commitment_context_init, bbs_blind_commitment_context_add_message_bytes, bbs_blind_commitment_context_add_message_prehashed, bbs_blind_commitment_context_set_public_key, bbs_blind_commitment_context_set_nonce_bytes, bbs_blind_commitment_context_finish, bbs_blind_signature_size};
 use crate::bbs_blind_sign::{bbs_blind_sign_context_init, bbs_blind_sign_context_set_secret_key, bbs_blind_sign_context_set_public_key, bbs_blind_sign_context_set_commitment, bbs_blind_sign_context_add_message_bytes, bbs_blind_sign_context_add_message_prehashed, bbs_blind_sign_context_finish, bbs_blinding_factor_size, bbs_unblind_signature};
+use crate::bbs_create_proof::{bbs_create_proof_context_init, bbs_create_proof_context_set_public_key, bbs_create_proof_context_set_signature, bbs_create_proof_context_set_nonce_bytes, bbs_create_proof_context_add_proof_message_bytes, bbs_create_proof_context_finish};
+use bbs::signature::SIGNATURE_COMPRESSED_SIZE;
 
 #[allow(non_snake_case)]
 #[no_mangle]
@@ -573,42 +575,110 @@ pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1unblind_1signature(env: JNIEnv,
     1
 }
 
-
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1init(env: JNIEnv, _: JObject) -> jlong {
-    0
+pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1init(_: JNIEnv, _: JObject) -> jlong {
+    let mut error = ExternError::success();
+    bbs_create_proof_context_init(&mut error) as jlong
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1set_1public_1key(env: JNIEnv, _: JObject, handle: jlong, public_key: jbyteArray) -> jint {
-    0
+    match env.convert_byte_array(public_key) {
+        Err(_) => 0,
+        Ok(s) => {
+            if s.len() < G2_COMPRESSED_SIZE {
+                0
+            } else {
+                let mut error = ExternError::success();
+                let byte_array = ByteArray::from(s);
+                bbs_create_proof_context_set_public_key(handle as u64, byte_array, &mut error)
+            }
+        }
+    }
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1set_1signature(env: JNIEnv, _: JObject, handle: jlong, signature: jbyteArray) -> jint {
-    0
+    match env.convert_byte_array(signature) {
+        Err(_) => 0,
+        Ok(s) => {
+            if s.len() < SIGNATURE_COMPRESSED_SIZE {
+                0
+            } else {
+                let mut error = ExternError::success();
+                let byte_array = ByteArray::from(s);
+                bbs_create_proof_context_set_signature(handle as u64, byte_array, &mut error)
+            }
+        }
+    }
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1set_1nonce_1bytes(env: JNIEnv, _: JObject, handle: jlong, nonce: jbyteArray) -> jint {
-    0
+    match env.convert_byte_array(nonce) {
+        Err(_) => 0,
+        Ok(s) => {
+            if s.len() < G2_COMPRESSED_SIZE {
+                0
+            } else {
+                let mut error = ExternError::success();
+                let byte_array = ByteArray::from(s);
+                bbs_create_proof_context_set_nonce_bytes(handle as u64, byte_array, &mut error)
+            }
+        }
+    }
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1add_1proof_1message_1bytes(env: JNIEnv, _: JObject, handle: jlong, message: jbyteArray, xtype: jint, blinding_factor: jbyteArray) -> jint {
-    0
+    match env.convert_byte_array(message) {
+        Err(_) => 0,
+        Ok(s) => {
+            let mut error = ExternError::success();
+            let byte_array = ByteArray::from(s);
+            let mut bf_byte_array = ByteArray::default();
+            let proof_msg_type = match xtype {
+                1 => ProofMessageType::Revealed,
+                2 => ProofMessageType::HiddenProofSpecificBlinding,
+                3 => {
+                    match env.convert_byte_array(blinding_factor) {
+                        Err(_) => return 0,
+                        Ok(bf) => {
+                            bf_byte_array = ByteArray::from(bf);
+                        }
+                    };
+                    ProofMessageType::HiddenExternalBlinding
+                },
+                _ => return 0
+            };
+            bbs_create_proof_context_add_proof_message_bytes(handle as u64, byte_array, proof_msg_type, bf_byte_array, &mut error)
+        }
+    }
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Java_bbs_signatures_Bbs_bbs_1create_1proof_1context_1finish(env: JNIEnv, _: JObject, handle: jlong) -> jbyteArray {
     let bad_res = env.new_byte_array(0).unwrap();
-    bad_res
+    let mut error = ExternError::success();
+    let mut p = ByteBuffer::from_vec(vec![]);
+    let res = bbs_create_proof_context_finish(handle as u64, &mut p, &mut error);
+    if res == 0 {
+        return bad_res;
+    }
+    let pp: Vec<jbyte> = p.into_vec().iter().map(|b| *b as jbyte).collect();
+    match env.new_byte_array(pp.len() as jint) {
+        Err(_) => env.new_byte_array(0).unwrap(),
+        Ok(out) => {
+            copy_to_jni!(env, out, pp.as_slice(), bad_res);
+            out
+        }
+    }
 }
 
 fn get_secret_key(env: &JNIEnv, secret_key: jbyteArray) -> Result<SecretKey, jint> {
