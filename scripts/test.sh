8047a62ad2ed2addb53b9f28d8a3b39947499f36
Margaryta <margaryta.kolomiyets@mattr.global>
2024-12-02 11:05:06 +1300

    fix: bump github actions, use arm arch for macos, add custom flag [skip ci objective-c wrapper]

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-LANGUAGE=$1
-PLATFORM=$2
+LANGUAGE=$1 #C
+PLATFORM=$2 #MACOS
 
 if [ -z "$LANGUAGE" ]
 then
@@ -20,13 +20,13 @@ fi
 
 case $PLATFORM in
   MACOS)
-      echo "Building for Apple Darwin x86_64"
-      rustup target add x86_64-apple-darwin
+      echo "Building for ARM Apple Darwin aarch64"
+      rustup target add aarch64-apple-darwin
       case $LANGUAGE in
         C)
           echo "To be used with C"
-          cargo build --target x86_64-apple-darwin --release
-          export RUST_LIBRARY_DIRECTORY="${PWD}/target/x86_64-apple-darwin/release"
+          cargo build --target aarch64-apple-darwin --release
+          export RUST_LIBRARY_DIRECTORY="${PWD}/target/aarch64-apple-darwin/release"
           cd $RUST_LIBRARY_DIRECTORY
           cmake ../../../tests
           cmake --build .
